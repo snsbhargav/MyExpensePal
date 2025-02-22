31835ebdd715bfae00994dc18eb6c6dcdd83d4a9
Bhargav Siddineni <snsbhargav@gmail.com>
2025-02-22 17:15:11 -0500

    Modified Authentication validateToken API to consume token from Header.

--- MyExpensePal Backend/AuthenticationService/src/main/java/com/MyExpensePal/AuthenticationService/Contoller/UserController.java
+++ MyExpensePal Backend/AuthenticationService/src/main/java/com/MyExpensePal/AuthenticationService/Contoller/UserController.java
@@ -1,14 +1,19 @@
 package com.MyExpensePal.AuthenticationService.Contoller;
 
+import java.net.http.HttpRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 
 import com.MyExpensePal.AuthenticationService.Dto.UserDto;
 import com.MyExpensePal.AuthenticationService.Dto.UserLoginDto;
@@ -18,25 +23,30 @@ import com.MyExpensePal.AuthenticationService.Service.UserService;
 @RestController
 @RequestMapping("/auth/")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@PostMapping("/register")
 	public ResponseEntity<UserDto> registerUser(@RequestBody UserEntity user) {
 		return userService.registerUser(user);
 	}
-	
+
 	@PostMapping("/login")
 	public ResponseEntity<String> loginUser(@RequestBody UserLoginDto loginDto) {
 		return userService.validateUser(loginDto);
 	}
-	
+
 	@PostMapping("/validateToken")
-	public ResponseEntity<Boolean> validateToken(@RequestParam String token){
+	public ResponseEntity<Boolean> validateToken(
+			@RequestHeader(value = "Authorization", required = false) String authHeader) {
+		if (authHeader == null || !authHeader.startsWith("Bearer "))
+			return new ResponseEntity<Boolean>(false, HttpStatus.UNAUTHORIZED);
+
+		String token = authHeader.substring(7);
 		return userService.validateToken(token);
 	}
-	
+
 	@GetMapping("test")
 	public String test() {
 		return "test";
