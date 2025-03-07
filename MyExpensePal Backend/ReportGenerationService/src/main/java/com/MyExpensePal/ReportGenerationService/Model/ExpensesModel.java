3b216346d4935424d65aae2c0cbaf176fc9db70d
Bhargav Siddineni <snsbhargav@gmail.com>
2025-03-06 21:05:59 -0500

    Updated the expense Model in report-generation-service.

--- MyExpensePal Backend/ReportGenerationService/src/main/java/com/MyExpensePal/ReportGenerationService/Model/ExpensesModel.java
+++ MyExpensePal Backend/ReportGenerationService/src/main/java/com/MyExpensePal/ReportGenerationService/Model/ExpensesModel.java
@@ -4,11 +4,18 @@ import java.util.Date;
 import java.util.UUID;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 public class ExpensesModel {
-	
-	public ExpensesModel() {
-		// TODO Auto-generated constructor stub
-	}
 	
 	private UUID expenseId;
 	private UUID userId;
@@ -16,100 +23,11 @@ public class ExpensesModel {
 	private Integer expense;
 	private ExpenseType expenseType;
 	private String location;
+	private TransactionType transactionType;
 	private Date date;
 	private Date time;
-	
-	public ExpensesModel(UUID expenseId, UUID userId, String expenseName, Integer expense, ExpenseType expenseType,
-			String location, Date date, Date time) {
-		super();
-		this.expenseId = expenseId;
-		this.userId = userId;
-		this.expenseName = expenseName;
-		this.expense = expense;
-		this.expenseType = expenseType;
-		this.location = location;
-		this.date = date;
-		this.time = time;
-	}
-
-	public UUID getExpenseId() {
-		return expenseId;
-	}
-
-	public void setExpenseId(UUID expenseId) {
-		this.expenseId = expenseId;
-	}
-
-	public UUID getUserId() {
-		return userId;
-	}
-
-	public void setUserId(UUID userId) {
-		this.userId = userId;
-	}
-
-	public String getExpenseName() {
-		return expenseName;
-	}
-
-	public void setExpenseName(String expenseName) {
-		this.expenseName = expenseName;
-	}
-
-	public Integer getExpense() {
-		return expense;
-	}
-
-	public void setExpense(Integer expense) {
-		this.expense = expense;
-	}
-
-	public ExpenseType getExpenseType() {
-		return expenseType;
-	}
-
-	public void setExpenseType(ExpenseType expenseType) {
-		this.expenseType = expenseType;
-	}
-
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-	public Date getDate() {
-		return date;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public Date getTime() {
-		return time;
-	}
-
-	public void setTime(Date time) {
-		this.time = time;
-	}
-
-	@Override
-	public String toString() {
-		return "ExpensesModel [expenseId=" + expenseId + ", userId=" + userId + ", expenseName=" + expenseName
-				+ ", expense=" + expense + ", expenseType=" + expenseType + ", location=" + location + ", date=" + date
-				+ ", time=" + time + "]";
-	}
-	
+	private Date createdOn;
+	private Date lastModified;
 	
 
-
-
-	
-	
-	
-	
-	
 }
