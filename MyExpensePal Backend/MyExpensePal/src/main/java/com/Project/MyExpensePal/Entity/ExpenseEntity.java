73f512e2da91179c12b03ea7accd1c95325f93ec
Bhargav Siddineni <snsbhargav@gmail.com>
2025-03-06 20:57:07 -0500

    Fixed bugs in Expense Service.

--- MyExpensePal Backend/MyExpensePal/src/main/java/com/Project/MyExpensePal/Entity/ExpenseEntity.java
+++ MyExpensePal Backend/MyExpensePal/src/main/java/com/Project/MyExpensePal/Entity/ExpenseEntity.java
@@ -2,10 +2,15 @@ package com.Project.MyExpensePal.Entity;
 
 
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import com.Project.MyExpensePal.Enum.ExpenseType;
 import com.Project.MyExpensePal.Enum.TransactionType;
 
@@ -19,6 +24,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
@@ -39,11 +45,12 @@ public class ExpenseEntity {
 	//Expense name, Expensetype(food, entertainment etc), picture, location, amount, credit/debit, datetime, reciepts
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
-	//USE UUID for expense Id
 	private UUID expenseId;
 	@Column(nullable = false)
 	private UUID userId;
+	@Column(nullable = false)
 	private String expenseName;
+	@Column(nullable = false)
 	private Integer expense;
 	//Add receipt
 	@Enumerated(EnumType.STRING)
@@ -52,13 +59,29 @@ public class ExpenseEntity {
 	@Enumerated(EnumType.STRING)
 	private TransactionType transactionType;
 	
-	@CreationTimestamp
 	@Temporal(TemporalType.DATE)
-	private Date date = new Date();
+	private LocalDate date;
+	
+	@Temporal(TemporalType.TIME)
+	private LocalTime time;
 	
 	@CreationTimestamp
-	@Temporal(TemporalType.TIME)
-	private Date time = new Date();
+	private Date createdOn;
+	
+	@UpdateTimestamp
+	private Date lastModified;
+	
+	@PrePersist
+    protected void onCreate() {
+        if (this.date == null) {
+            this.date = LocalDate.now(ZoneId.of("UTC")); // Set UTC time zone
+        }
+        if (this.time == null) {
+            this.time = LocalTime.now(ZoneId.of("UTC"));
+        }
+    }
+	
+	
 	
 	//Add receipts
 }
